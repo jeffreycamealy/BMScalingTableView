075eb4b1dcfb165f91dc82966c81f0cab5a13b2f
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-12-12 13:16:24 -0600

    Initial Commit

---
+++ BMScalingTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BMScalingTableView
+//
+//  Created by Jeffrey Camealy on 12/12/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
